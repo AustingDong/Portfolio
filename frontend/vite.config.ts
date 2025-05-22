d3ea4ce0097db5651f34673f1bf9bc2d0c414c73
AustingDong <a7dong@uwaterloo.ca>
2025-05-22 00:56:20 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // vite.config.ts
 export default defineConfig({
-  base: '/portfolio/', // 注意必须是你的 GitHub 仓库名
+  base: '/Portfolio/', // 注意必须是你的 GitHub 仓库名
   plugins: [react()],
 });
